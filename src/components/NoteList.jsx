72a982222d3c64e97047f253c3b25417f53fb019
Ildar060493 <146338590+Ildar060493@users.noreply.github.com>
2025-03-26 17:58:42 +0500

    Update NoteList.jsx

--- src/components/NoteList.jsx
+++ src/components/NoteList.jsx
@@ -1,1 +1,18 @@
+import React, { useContext } from "react";
+import { DataContext } from "../DataContext";
+import NoteCard from "./NoteCard";
+
+function NoteList() {
+  const { tasks } = useContext(DataContext);
+
+  return (
+    <div className="note-list">
+      {tasks.map((note, index) => (
+        <NoteCard key={index} {...note} />
+      ))}
+    </div>
+  );
+}
+
+export default NoteList;
 
