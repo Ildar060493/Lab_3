9abafe12a10ce15325b1942992d282bbb6b15bba
Ildar060493 <146338590+Ildar060493@users.noreply.github.com>
2025-03-26 18:45:38 +0500

    Update DataContext.jsx

--- src/DataContext.jsx
+++ src/DataContext.jsx
@@ -1,47 +1,16 @@
+// src/DataContext.jsx
+
 import React, { createContext, useState } from "react";
 
 export const DataContext = createContext();
 
 export const DataProvider = ({ children }) => {
-    const [tasks, setTasks] = useState([
-        {
-            id: 1,
-            content: "HTML is easy",
-            date: {
-                day: "2019-05-30",
-                start: "09:00",
-                end: "10:00"
-            },
-            important: true,
-            completed: false
-        },
-        {
-            id: 2,
-            content: "Browser can execute only Javascript",
-            date: {
-                day: "2019-05-30",
-                start: "10:00",
-                end: "11:00"
-            },
-            important: false,
-            completed: true
-        },
-        {
-            id: 3,
-            content: "GET and POST are the most important methods of HTTP protocol",
-            date: {
-                day: "2019-05-31",
-                start: "11:00",
-                end: "12:00"
-            },
-            important: true,
-            completed: false
-        }
-    ]);
+  const [tasks, setTasks] = useState([]);
 
-    return (
-        <DataContext.Provider value={{ tasks, setTasks }}>
-            {children}
-        </DataContext.Provider>
-    );
+  return (
+    <DataContext.Provider value={{ tasks, setTasks }}>
+      {children}
+    </DataContext.Provider>
+  );
 };
+
