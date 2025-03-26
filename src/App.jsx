1e1d261c7fb6285fe0e207a297312dfa9f2eb114
Ildar060493 <146338590+Ildar060493@users.noreply.github.com>
2025-03-26 18:01:42 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { DataProvider } from "./DataContext";
 import NoteList from "./components/NoteList";
+import NoteAdd from "./components/NoteAdd";
 
 function App() {
   return (
     <DataProvider>
       <div className="container">
         <h1>To-Do List</h1>
+        <NoteAdd />
         <NoteList />
       </div>
     </DataProvider>
