5d556481f68714f6c4942d2aa898cc338cda2f7a
Ildar060493 <146338590+Ildar060493@users.noreply.github.com>
2025-03-26 18:38:03 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
-import React from 'react';
-import { DataProvider } from './DataContext';
-import NoteAdd from './components/NoteAdd';
-import NoteList from './components/NoteList';
-import './index.css';
+// src/App.jsx
+
+import React from "react";
+import { DataProvider } from "./DataContext";
+import NoteAdd from "./components/NoteAdd";
+import NoteList from "./components/NoteList";
 
 function App() {
-    return (
-        <DataProvider>
-            <div className="container">
-                <NoteAdd />
-                <NoteList />
-            </div>
-        </DataProvider>
-    );
+  return (
+    <DataProvider>
+      <div className="container">
+        <NoteAdd />
+        <NoteList />
+      </div>
+    </DataProvider>
+  );
 }
 
 export default App;
+
 
