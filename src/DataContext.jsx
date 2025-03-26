6b842ed457518e8890f0980af7997b8d25f75f0a
Ildar060493 <146338590+Ildar060493@users.noreply.github.com>
2025-03-26 18:17:57 +0500

    Update DataContext.jsx

--- src/DataContext.jsx
+++ src/DataContext.jsx
@@ -1,26 +1,36 @@
-import { createContext, useState } from "react";
+import React, { createContext, useState } from 'react';
 
 export const DataContext = createContext();
 
 export function DataProvider({ children }) {
-  const [tasks, setTasks] = useState([
-    {
-      content: "Изучить JSX",
-      date: { day: "2025-03-26", start: "10:00", end: "11:00" },
-      important: true,
-      completed: false,
-    },
-    {
-      content: "Разобраться с useState",
-      date: { day: "2025-03-26", start: "11:00", end: "12:00" },
-      important: false,
-      completed: true,
-    },
-  ]);
+    const [tasks, setTasks] = useState([
+        {
+            id: 1,
+            content: "HTML is easy",
+            date: {
+                day: "2025-03-26",
+                start: "09:00",
+                end: "10:00"
+            },
+            important: true,
+            completed: false
+        },
+        {
+            id: 2,
+            content: "Browser can execute only Javascript",
+            date: {
+                day: "2025-03-26",
+                start: "10:00",
+                end: "11:00"
+            },
+            important: false,
+            completed: true
+        }
+    ]);
 
-  return (
-    <DataContext.Provider value={{ tasks, setTasks }}>
-      {children}
-    </DataContext.Provider>
-  );
+    return (
+        <DataContext.Provider value={{ tasks, setTasks }}>
+            {children}
+        </DataContext.Provider>
+    );
 }
